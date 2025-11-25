89a3f5e526e333b163b739a6e3090e94b9001b59
michaelreed452 <michaelreed452@gmail.com>
2025-11-25 15:34:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ st.sidebar.success(f"Logged in as **{st.session_state.user.upper()}**")
 
 st.set_page_config(
     page_title="Grow Tracker",
-    page_icon="https://em-content.zobj.net/source/apple/391/seedling_1f331.png",  # real green leaf
+    page_icon="https://raw.githubusercontent.com/michaelreed452/cannabis-grow-tracker/main/leaf.png",
     layout="wide",
     initial_sidebar_state="expanded"
 )
